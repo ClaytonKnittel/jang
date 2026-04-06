c4d4e468d241e933ec04364e9ba2e6973d72e0f6
Clayton Knittel <cknit1999@gmail.com>
2026-04-05 23:46:04 -0700

    FromVariants

--- src/parser/lexer.rs
+++ src/parser/lexer.rs
@@ -8,7 +8,7 @@ use crate::{
     JangToken,
     ident::Ident,
     keyword::Keyword,
-    literal::NumericLiteral,
+    literal::{Literal, NumericLiteral},
     operator::{Op, Operator},
   },
   source_location::SourceLocation,
@@ -74,7 +74,7 @@ impl<I: Iterator<Item = char>> TokenIter<I> {
 
   fn parse_numeric(&mut self, first_char: char) -> JangToken {
     let numeric = self.collect_while(first_char, is_numeric_char);
-    NumericLiteral::from_str(numeric).into()
+    Literal::from(NumericLiteral::from_str(numeric)).into()
   }
 
   fn parse_operator(&mut self, first_char: char) -> JangToken {
