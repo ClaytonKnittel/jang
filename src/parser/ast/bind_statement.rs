40f0535c83fe0253cf1674d28f3032ee166c8155
ClaytonKnittel <35512940+ClaytonKnittel@users.noreply.github.com>
2026-04-16 01:15:03 -0700

    Type annotations on let/mut bindings (#62)

--- src/parser/ast/bind_statement.rs
+++ src/parser/ast/bind_statement.rs
@@ -1,6 +1,8 @@
 use std::fmt::Display;
 
-use crate::parser::ast::{expression::Expression, var::var_decl::LocalDecl};
+use crate::parser::ast::{
+  expression::Expression, type_expr::TypeExpression, var::var_decl::LocalDecl,
+};
 
 #[derive(Copy, Clone, Debug)]
 pub enum Mutability {
@@ -12,22 +14,25 @@ pub enum Mutability {
 pub struct BindStatement {
   mutability: Mutability,
   var: LocalDecl,
+  var_type: Option<TypeExpression>,
   expr: Expression,
 }
 
 impl BindStatement {
-  pub fn new_let(var: LocalDecl, expr: Expression) -> Self {
+  pub fn new_let(var: LocalDecl, var_type: Option<TypeExpression>, expr: Expression) -> Self {
     Self {
       mutability: Mutability::Immutable,
       var,
+      var_type,
       expr,
     }
   }
 
-  pub fn new_mut(var: LocalDecl, expr: Expression) -> Self {
+  pub fn new_mut(var: LocalDecl, var_type: Option<TypeExpression>, expr: Expression) -> Self {
     Self {
       mutability: Mutability::Mutable,
       var,
+      var_type,
       expr,
     }
   }
@@ -38,6 +43,10 @@ impl BindStatement {
 
   pub fn var(&self) -> &LocalDecl {
     &self.var
+  }
+
+  pub fn var_type(&self) -> Option<&TypeExpression> {
+    self.var_type.as_ref()
   }
 
   pub fn expr(&self) -> &Expression {
@@ -61,6 +70,7 @@ pub(crate) mod matchers {
       bind_statement::{BindStatement, Mutability},
       expression::Expression,
       statement::Statement,
+      type_expr::TypeExpression,
       var::var_decl::matchers::local_decl,
     },
     token::ident::Ident,
@@ -72,9 +82,23 @@ pub(crate) mod matchers {
     expr_matcher: impl Matcher<&'a Expression>,
   ) -> impl Matcher<&'a Statement> {
     pat!(Statement::Bind(pat!(BindStatement {
+      mutability: pat!(Mutability::Immutable),
       var: local_decl(var_matcher),
+      var_type: none(),
       expr: expr_matcher,
-      mutability: pat!(Mutability::Immutable)
+    })))
+  }
+
+  pub fn let_stmt_with_type<'a>(
+    var_matcher: impl Matcher<&'a Ident>,
+    type_matcher: impl Matcher<&'a TypeExpression>,
+    expr_matcher: impl Matcher<&'a Expression>,
+  ) -> impl Matcher<&'a Statement> {
+    pat!(Statement::Bind(pat!(BindStatement {
+      mutability: pat!(Mutability::Immutable),
+      var: local_decl(var_matcher),
+      var_type: some(type_matcher),
+      expr: expr_matcher,
     })))
   }
 
@@ -83,9 +107,23 @@ pub(crate) mod matchers {
     expr_matcher: impl Matcher<&'a Expression>,
   ) -> impl Matcher<&'a Statement> {
     pat!(Statement::Bind(pat!(BindStatement {
+      mutability: pat!(Mutability::Mutable),
       var: local_decl(var_matcher),
+      var_type: none(),
       expr: expr_matcher,
-      mutability: pat!(Mutability::Mutable)
+    })))
+  }
+
+  pub fn mut_stmt_with_type<'a>(
+    var_matcher: impl Matcher<&'a Ident>,
+    type_matcher: impl Matcher<&'a TypeExpression>,
+    expr_matcher: impl Matcher<&'a Expression>,
+  ) -> impl Matcher<&'a Statement> {
+    pat!(Statement::Bind(pat!(BindStatement {
+      mutability: pat!(Mutability::Mutable),
+      var: local_decl(var_matcher),
+      var_type: some(type_matcher),
+      expr: expr_matcher,
     })))
   }
 }
