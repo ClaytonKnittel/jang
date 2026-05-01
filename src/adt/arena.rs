8ecd05435718a456491b4d3a8e372f17dc228148
jackson <jtstogel@berkeley.edu>
2026-04-30 20:33:25 -0700

    More comments

--- src/adt/arena.rs
+++ src/adt/arena.rs
@@ -1,52 +1,61 @@
-use std::cell::{RefCell, RefMut};
+use std::cell::UnsafeCell;
+
+const CHUNK_CAPACITY: usize = 512;
 
 /// A basic arena that provides stable references to stored items.
-/// [`Arena`] is not thread-safe.
 pub struct Arena<T> {
-  chunks: RefCell<Vec<Chunk<T>>>,
+  chunks: UnsafeCell<Vec<Chunk<T>>>,
 }
 
 impl<T> Default for Arena<T> {
   fn default() -> Self {
     Self {
-      chunks: RefCell::new(vec![Chunk::default()]),
+      chunks: UnsafeCell::new(vec![Chunk::new()]),
     }
   }
 }
 
 impl<T> Arena<T> {
-  pub fn alloc(&self, value: T) -> &T {
-    let mut chunk = self.chunk_with_capacity();
-    let entry: &T = chunk.push(value);
+  const NON_EMPTY: &str = "chunks are non-empty, new() creates a chunk";
 
-    // Extend the lifetime of the reference to `entry`.
+  /// Stores and provides a reference to `value`.
+  pub fn alloc(&self, value: T) -> &T {
+    // This method mutates `chunks` and returns a reference to its content.
     //
     // This is safe because:
-    // - The lifetime of the returned reference is bound to this Arena,
-    //   so it cannot outlive the chunk holding its data.
-    // - A chunk's underlying Vec is never expanded beyond its capacity,
-    //   so it is guarenteed to never reallocate.
+    //  - The lifetime of the returned reference is bound to this Arena,
+    //    so it cannot outlive the chunk holding its data.
+    //  - We specifically avoid invalidating any of the previous
+    //    references produced by `alloc`, so it is safe to mutate `chunks`.
     //
-    // Vec actually makes quite a few guarentees about how and where
-    // its elements are allocated. From the docs:
-    //  - Vec is and always will be a (pointer, capacity, length) triplet.
-    //  - The memory it points to is on the heap.
-    //  - `push` and `insert` will never (re)allocate if the reported
-    //    capacity is sufficient.
-    //  - Vec will never perform a "small optimization" where elements are
-    //    actually stored on the stack... [because] the contents of a Vec
-    //    wouldn’t have a stable address if it were only moved.
-    unsafe { std::mem::transmute(entry) }
-  }
+    // There are some pathological cases Arena is unsafe for:
+    //  - If `value` itself has references to this Arena, and its `Drop`
+    //    impl tries to follow those references, they may be invalid
+    //    by the time `Vec` gets around to dropping the `value`.
+    //    `Vec` makes no guarantee on order of `drop` calls.
+    let chunks: &mut Vec<Chunk<T>> = unsafe { &mut *self.chunks.get() };
 
-  fn chunk_with_capacity(&self) -> RefMut<'_, Chunk<T>> {
-    RefMut::map(self.chunks.borrow_mut(), |chunks| {
-      let last_chunk = chunks.last().expect("Arena::default creates a chunk");
-      if last_chunk.full() {
-        chunks.push(Chunk::default());
-      }
-      chunks.last_mut().expect("Arena::default creates a chunk")
-    })
+    // If the last chunk has no space, create another one.
+    if chunks.last().expect(Self::NON_EMPTY).is_full() {
+      // While this may cause chunks to resize, this is okay
+      // because `Chunk` maintains pointer stability on moves.
+      //
+      // `Chunk` is just a Vec wrapper, and Vec's docs state:
+      //  - Vec is and always will be a (pointer, capacity, length) triplet.
+      //  - The memory it points to is on the heap.
+      //  - Vec will never perform a "small optimization" where elements are
+      //    actually stored on the stack... [because] the contents of a Vec
+      //    wouldn't have a stable address if it were only moved.
+      chunks.push(Chunk::new());
+    }
+
+    // Append to the last chunk.
+    //
+    // This is guaranteed to not affect stability of previously allocated
+    // references, because the Vec documentation states that "push and insert
+    // will never (re)allocate if the reported capacity is sufficient."
+    // We have checked the capacity above, so this is fine.
+    chunks.last_mut().expect(Self::NON_EMPTY).push(value)
   }
 }
 
@@ -55,23 +64,19 @@ struct Chunk<T> {
   values: Vec<T>,
 }
 
-const CHUNK_SIZE: usize = 512;
-
-impl<T> Default for Chunk<T> {
-  fn default() -> Self {
+impl<T> Chunk<T> {
+  fn new() -> Self {
     Self {
-      values: Vec::with_capacity(CHUNK_SIZE),
+      values: Vec::with_capacity(CHUNK_CAPACITY),
     }
   }
-}
 
-impl<T> Chunk<T> {
-  fn full(&self) -> bool {
+  fn is_full(&self) -> bool {
     self.values.len() == self.values.capacity()
   }
 
   fn push(&mut self, value: T) -> &T {
-    debug_assert!(!self.full());
+    debug_assert!(!self.is_full());
     self.values.push(value);
     self.values.last().expect("just pushed")
   }
@@ -81,13 +86,13 @@ impl<T> Chunk<T> {
 mod tests {
   use googletest::prelude::*;
 
-  use crate::adt::arena::{Arena, CHUNK_SIZE};
+  use crate::adt::arena::{Arena, CHUNK_CAPACITY};
 
   #[gtest]
   fn test_alloc_new_chunk() {
     let arena = Arena::default();
     let mut refs = Vec::new();
-    for i in 0..(64 * CHUNK_SIZE) {
+    for i in 0..(64 * CHUNK_CAPACITY) {
       refs.push(arena.alloc(i));
     }
     for (i, r) in refs.into_iter().enumerate() {
